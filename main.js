7c3c2995361967c5c40621446f11fc599de7d536
ZmazingUzumaki <122739235+ZmazingUzumaki@users.noreply.github.com>
2023-01-16 17:21:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(500, 500);
+    canvas.center();
+}
+
+function draw(){
+ Image
+}
+
+function take_snapshot(){
+
+    save('myprojectfilterpic.png')
+
+}
